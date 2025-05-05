352c7bdb0a4001b093750f0e4f679161b5b76394
231172 <231172@CPD02-M14>
2025-05-05 15:54:53 -0300

    alteracoes

---
+++ Controller/Controller/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Model;
+
+abstract class Controller 
+{
+    final protected static function isProtected() : void 
+    {
+        if(isset($_SESSION['usuario_logado']))
+            header("Location: /login");
+    }
+
+    final protected static function render(string $view, ?model $model) : void 
+    {
+        include views . "/$views";
+    }
+
+    final protected static function isPost() : bool
+    {
+        return $_SERVER['REQUEST_METHOD'] =="POST";
+    }
+
+    final protected static function redirect(string $route) : void
+    {
+        header("Location: $route");
+    }
+}
+
